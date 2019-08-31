3a8a9a0a747451e88dc8d233efbf5167a1928ad2
NehaParmar06 <neha.parmar06@gmail.com>
2019-08-31 07:09:21 +0530

    Part-A Create Comment Modal

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-
+// Get the Create Post modal
+var modal = document.getElementById("createPostModal");
+var btn = document.getElementById("createPostButton");
+var span = document.getElementsByClassName("close")[2];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+document.getElementById("createPostHeading").innerHTML = "Pen Your Post";
